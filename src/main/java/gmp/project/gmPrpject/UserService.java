8a94c5dc093b797038af80beb59fcedf3b2134f2
Arturs <arsv@inbox.lv>
2020-04-13 14:53:50 +0300

    Initial commit

---
+++ src/main/java/gmp/project/gmPrpject/UserService.java
@@ -0,0 +1,12 @@
+package gmp.project.gmPrpject;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+
+public interface UserService extends UserDetailsService {
+
+    User findByEmail(String email);
+
+    User save(UserRegistrationDto registration);
+}
---
+++ src/test/java/gmp/project/gmPrpject/GmPrpjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gmp.project.gmPrpject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmPrpjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
